7982a04704facc431ab91e3403d24cfb1e45cac8
rijaluddina <rjlddn.a@gmail.com>
2026-01-11 01:22:28 +0700

    bump:analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
         <main className="min-h-screen p-8 flex flex-col items-center justify-center">
           {children}
         </main>
+        <Analytics />
       </body>
     </html>
   );
